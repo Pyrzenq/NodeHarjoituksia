033199351c0ce0d14760279cab19ed47364e8a10
Pyrzenq <111853294+Pyrzenq@users.noreply.github.com>
2023-09-25 22:54:40 +0300

    Added main program app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+// WEB SERVER FOR ELECTRICITY USAGE PLANNING: WEB PAGES AND AN API
+// ===============================================================
+
+// LIBRARIES AND MODULES
+// =====================
+
+// Use Express as web engine
+const express = require('express');
+// Use Express Handlebars as template engine
+const {engine} = require('express-handlebars');
+
+// EXPRESS APPLICATION SETTINGS
+// ----------------------------
+const app = express();
+const PORT = process.env.PORT || 8080
+
+// Set folder paths
+app.use(express.static('public'));
+app.set('views', './views');
+
+// Engine settings
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+// URL ROUTES
+// ----------
+
+// START THE LISTENER
+app.listen(PORT);
+console.log('Server started and it will listen TCP port', PORT);
