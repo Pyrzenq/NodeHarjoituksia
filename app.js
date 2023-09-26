152884b132ab1a233619c8e71d574718598a1ea0
Pyrzenq <111853294+Pyrzenq@users.noreply.github.com>
2023-09-26 12:48:49 +0300

    Added styles for tables

--- app.js
+++ app.js
@@ -28,22 +28,31 @@ app.set('view engine', 'handlebars');
 // Route to home page
 app.get('/', (req, res) => {
 
+    // Handlebars needs a key to show data on a page, json is a good way to send it
     let homePageData = {
         'price': 31.25,
         'wind': 2,
         'temperature': 18
     }
+
+    // Render index.handlebars and send dynamic data to the page
     res.render('index', homePageData)
 
 });
 
+// Route to hourly data page
 app.get('/hourly', (req, res) => {
 
-    let hourlyPageDate = { 'tabledata': [
+    // Data will be presented in a table. To loop all rows we need a key for table and for column data
+    let hourlyPageDate = { 'tableData': [
         {'hour': 13,
         'price': 31.44},
-        {},
-        {},
+        {'hour': 14,
+        'price': 32.10},
+        {'hour': 15,
+        'price': 30.50},
+        {'hour': 16,
+        'price': 29.99}
     ]
     };
 
