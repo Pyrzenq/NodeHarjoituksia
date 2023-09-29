6741964962926d2fa073ba16781c5ad35e75a446
Pyrzenq <111853294+Pyrzenq@users.noreply.github.com>
2023-09-29 15:51:54 +0300

    sql harjoituksia
    sql harjoituksia

---
+++ AddHourlyPrices.sql
@@ -0,0 +1,5 @@
+-- Add a row (record) to hourly_prices table
+-- All field (columns) will get a value
+
+INSERT INTO public.hourly_price VALUES ('2023-08-14T11:00:00Z', 10);
+INSERT INTO public.hourly_price VALUES ('2023-08-14T12:00:00Z', 30);
